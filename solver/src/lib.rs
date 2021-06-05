6ecd41f4a71883f37fad481740fad98b62244602
nkalupahana <avishkank@gmail.com>
2021-06-05 00:13:27 -0700

    Basic interface all working!
    TODO:
    - "LI" as a vertical letter doesn't display correctly
    - Can't play multiple times
    - Maybe should suggest words that end with S to drop the S ending? Some sort of Trie search
    - Build and deploy

--- solver/src/lib.rs
+++ solver/src/lib.rs
@@ -2,13 +2,34 @@ mod trie;
 mod dictionary;
 use trie::Trie;
 use dictionary::SCRABBLE_DICTIONARY;
-use std::collections::BTreeSet;
 use wasm_bindgen::prelude::*;
+use serde::{Serialize, Deserialize};
+extern crate console_error_panic_hook;
 
 #[derive(Clone, Copy)]
 enum SolveDirection {
     Horizontal,
     Vertical
+}
+
+#[derive(Clone, Copy, Serialize, Deserialize)]
+enum LetterType {
+    Single,
+    Horizontal,
+    Vertical
+}
+
+#[derive(Clone, Serialize, Deserialize)]
+struct LetterBox {
+    ltype: LetterType,
+    letters: [String; 2],
+    pos: u8
+}
+
+#[derive(Serialize, Deserialize)]
+struct WordResult {
+    combination: Vec<LetterBox>,
+    word: String
 }
 
 fn read_dict(trie: &mut Trie){
@@ -17,7 +38,7 @@ fn read_dict(trie: &mut Trie){
     }
 }
 
-fn find_words(direction: SolveDirection, legal_words: &Trie, found_words: &mut Trie, word_set: &mut BTreeSet<String>, prefix: &str, remaining_letters: &str) {
+fn find_words(direction: SolveDirection, legal_words: &Trie, found_words: &mut Trie, word_vec: &mut Vec<WordResult>, prefix: &str, prefix_vec: &mut Vec<LetterBox>, remaining_letters: &str) {
     // If not a prefix of a word, don't explore any further
     if !legal_words.is_prefix(prefix) {
         return;
@@ -29,9 +50,12 @@ fn find_words(direction: SolveDirection, legal_words: &Trie, found_words: &mut T
     }
 
     // If this is a word and it's longer than 3 characters, insert it
-    if legal_words.is_word(prefix) && prefix.len() >= 3 {
+    if legal_words.is_word(prefix) && prefix.len() == 7 {
         found_words.insert(prefix);
-        word_set.insert(String::from(prefix));
+        word_vec.push(WordResult {
+            combination: prefix_vec.clone(),
+            word: prefix.to_string()
+        });
     }
 
     // For each letter we have left to explore,
@@ -43,27 +67,48 @@ fn find_words(direction: SolveDirection, legal_words: &Trie, found_words: &mut T
         let letter = tvec.remove(i);
         let newstr = tvec.join(",");
         if letter.chars().count() == 1 {
-            find_words(direction, legal_words, found_words, word_set, &(String::from(prefix) + letter), &newstr);
+            prefix_vec.push(LetterBox {
+                ltype: LetterType::Single,
+                letters: [letter.to_string(), "".to_string()],
+                pos: 0
+            });
+            find_words(direction, legal_words, found_words, word_vec, &(String::from(prefix) + letter), prefix_vec, &newstr);
+            prefix_vec.pop();
         } else {
             // If this is a 2x1 or 1x2 block, explore it based on the SolveDirection
-            let mut solve_strategy = ["", ""];
+            let mut solve_strategy = ["-", "|"];
+            let mut solve_strategy_e = [LetterType::Horizontal, LetterType::Vertical];
             match direction {
                 SolveDirection::Horizontal => {
-                    solve_strategy[0] = "-";
-                    solve_strategy[1] = "|";
+                    // Default
                 },
                 SolveDirection::Vertical => {
-                    solve_strategy[0] = "|";
-                    solve_strategy[1] = "-";
+                    solve_strategy.reverse();
+                    solve_strategy_e.reverse();
                 }
             }
             
             if letter.contains(solve_strategy[0]) {
-                find_words(direction, legal_words, found_words, word_set, &(String::from(prefix) + &letter.split(solve_strategy[0]).collect::<Vec<&str>>().join("")), &newstr);
+                let letters: Vec<&str> = letter.split(solve_strategy[0]).collect();
+                assert_eq!(letters.len(), 2);
+                prefix_vec.push(LetterBox {
+                    ltype: solve_strategy_e[0],
+                    letters: [letters[0].to_string(), letters[1].to_string()],
+                    pos: 0
+                });
+                find_words(direction, legal_words, found_words, word_vec, &(String::from(prefix) + &letters.join("")), prefix_vec, &newstr);
+                prefix_vec.pop();
             } else if letter.contains(solve_strategy[1]) {
                 let letters: Vec<&str> = letter.split(solve_strategy[1]).collect();
-                for letter in &letters {
-                    find_words(direction, legal_words, found_words, word_set, &(String::from(prefix) + letter), &newstr);
+                assert_eq!(letters.len(), 2);
+                for (i, &letter) in letters.iter().enumerate() {
+                    prefix_vec.push(LetterBox {
+                        ltype: solve_strategy_e[1],
+                        letters: [letters[0].to_string(), letters[1].to_string()],
+                        pos: i as u8
+                    });
+                    find_words(direction, legal_words, found_words, word_vec, &(String::from(prefix) + letter), prefix_vec, &newstr);
+                    prefix_vec.pop();
                 }
             }
         }
@@ -71,7 +116,8 @@ fn find_words(direction: SolveDirection, legal_words: &Trie, found_words: &mut T
 }
 
 #[wasm_bindgen]
-pub fn run(letters: &str) -> String {
+pub fn run(letters: &str) -> wasm_bindgen::JsValue {
+    console_error_panic_hook::set_once();
     // Create Trie with scrabble dictionary
     let mut legal_words = Trie::new();
     read_dict(&mut legal_words);
@@ -79,14 +125,10 @@ pub fn run(letters: &str) -> String {
     // Find words
     let mut found_words_horiz = Trie::new();
     let mut found_words_vert = Trie::new();
-    let mut word_set: BTreeSet<String> = BTreeSet::new();
-    //find_words(SolveDirection::Horizontal, &legal_words, &mut found_words_horiz, &mut word_set, "", letters);
-    find_words(SolveDirection::Vertical, &legal_words, &mut found_words_vert, &mut word_set, "", letters);
+    let mut word_vec: Vec<WordResult> = Vec::new();
+    let mut prefix_vec: Vec<LetterBox> = Vec::new();
+    find_words(SolveDirection::Horizontal, &legal_words, &mut found_words_horiz, &mut word_vec, "", &mut prefix_vec, letters);
+    //find_words(SolveDirection::Vertical, &legal_words, &mut found_words_vert, &mut word_vec, "", &mut prefix_vec, letters);
 
-    // TreeSet -> Vec -> String, and return
-    let mut word_vect: Vec<String> = Vec::with_capacity(word_set.len());
-    for word in &word_set {
-        word_vect.push(word.to_string());
-    } 
-    return String::from(word_vect.join(","))
+    JsValue::from_serde(&word_vec).unwrap()
 }
